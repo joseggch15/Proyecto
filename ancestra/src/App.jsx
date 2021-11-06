366a1c66448dfac45c09593f67b8b0fbd414b3e1
Jose Gomez <josegch15@gmail.com>
2021-11-05 19:52:27 -0500

    Se hace un test del header

--- ancestra/src/App.jsx
+++ ancestra/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 function App() {
   return (
   <div className="App">
+    </Router>
+      
+     <Switch>
+          <Header/>
+        </Switch>
     <Footer/>
   </div>
   );
